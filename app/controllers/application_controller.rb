3dbfb67597b8e2b749e70cb8d77c835dca8a52a8
Spencer Vernon <svernon0@chicagobooth.edu>
2023-04-10 19:11:23 +0000

    Add square root and random

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,42 @@ class ApplicationController < ActionController::Base
   end
 
   def calculate_square
+    
+    @num = params.fetch("number").to_f
+    @sqaure_of_num = (@num ** 2)
 
     render({ :template => "calculation_templates/square_results.html.erb" })
+
+  end
+
+  def blank_square_root_form
+    
+    render({ :template => "calculation_templates/square_root_form.html.erb"})
+
+  end
+
+  def calculate_square_root
+
+    @user_num = params.fetch("user_number").to_f
+    @square_root_of_user_num = Math.sqrt(@user_num)
+
+    render({ :template => "calculation_templates/square_root_results.html.erb"})
+
+  end
+
+  def blank_random_form
+
+    render({ :template => "calculation_templates/random_form.html.erb"})
+
+  end
+
+  def calculate_random
+
+    @lower = params.fetch("user_min").to_f
+    @upper = params.fetch("user_max").to_f
+    @result = rand(@lower..@upper)
+
+    render({ :template => "calculation_templates/random_results.html.erb"})
 
   end
 
