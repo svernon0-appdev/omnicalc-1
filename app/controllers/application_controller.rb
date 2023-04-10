790ff56dd87802eda13a5e06aafca198de6a19db
Spencer Vernon <svernon0@chicagobooth.edu>
2023-04-10 18:36:35 +0000

    Initial edits

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
 
+  def blank_square_form
+
+    render({ :template => "calculation_templates/square_form.html.erb" })
+
+  end
+
+  def calculate_square
+
+    render({ :template => "calculation_templates/square_results.html.erb" })
+
+  end
+
 end
